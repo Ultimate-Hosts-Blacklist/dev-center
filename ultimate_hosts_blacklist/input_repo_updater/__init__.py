51c0748a4c4c021741cf812670f9f4fa7fd7f3e0
funilrys <contact@funilrys.com>
2019-04-18 23:57:17 +0200

    Precise pip to use

--- ultimate_hosts_blacklist/input_repo_updater/__init__.py
+++ ultimate_hosts_blacklist/input_repo_updater/__init__.py
@@ -39,7 +39,7 @@ from colorama import init as initiate_coloration
 
 from ultimate_hosts_blacklist.input_repo_updater.core import Core
 
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 
 def _command_line():
