8e98cd1f7757caa4ca73448513ed927629d54743
funilrys <contact@funilrys.com>
2019-05-15 14:24:26 +0200

    Update package name.
    Indeed, as the 2.x.x branch of PyFunceble was merged into the dev branch, we can now install the latest PyFunceble-dev package from PyPi instead of git.
    
    cc @mitchellkrogza @dnmTX
    
    Ping: Ultimate-Hosts-Blacklist/dev-center#32

--- ultimate_hosts_blacklist/input_repo_updater/configuration.py
+++ ultimate_hosts_blacklist/input_repo_updater/configuration.py
@@ -135,10 +135,7 @@ class PyFunceble:  # pylint: disable=too-few-public-methods
     }
 
     # Set the PyFunceble packages to install.
-    packages = {
-        "stable": "PyFunceble",
-        "dev": "git+https://github.com/funilrys/PyFunceble.git@2.x.x",
-    }
+    packages = {"stable": "PyFunceble", "dev": "PyFunceble-dev"}
 
 
 class Infrastructure:  # pylint: disable=too-few-public-methods
